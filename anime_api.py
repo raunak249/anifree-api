bbc30bbb1c5832ab92700cabc8818ee61b59d22f
Raunak Adak <raunak249@gmail.com>
2020-04-06 18:20:52 +0530

    changed anime home to gogoanime

--- anime_api.py
+++ anime_api.py
@@ -4,10 +4,10 @@ import requests
 from flask import Flask, jsonify, make_response,request
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import os
 
-ROOT_URL = 'https://animekisa.tv'
+
+ROOT_URL = 'https://www.gogoanime.io'
 CHROMEDRIVER_PATH = os.environ.get("CHROMEDRIVER_PATH")
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
@@ -15,7 +15,7 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sanbox")
 chrome_options.binary_location = '/app/.apt/usr/bin/google-chrome'
-#"C:/Windows/chromedriver.exe"
+
 '''
 Anime :
     anime_name
@@ -31,6 +31,8 @@ Episode :
     video_link
 '''
 
+
+
 def search_anime(anime_name):
     search_url = 'https://animekisa.tv/search?q=' + str(anime_name)
     response = requests.get(search_url)
@@ -45,97 +47,65 @@ def get_recent_anime():
     '''
     To get 5 most recently released anime
     '''
-    recent_anime = []
-    div_classes = ['episode-box test','episode-box test ep-second','episode-box test ep-third','episode-box test ep-second ep-third']
+    recent_animes = []
     url = ROOT_URL
     response = requests.get(url)
     soup = BeautifulSoup(response.content)
-    anime_divs = []
-    anime_divs.append(soup.find_all('div',{'class' : div_classes[0]})[0])
-    anime_divs.append(soup.find_all('div',{'class' : div_classes[1]})[0])
-    anime_divs.append(soup.find_all('div',{'class' : div_classes[2]})[0])
-    anime_divs.append(soup.find_all('div',{'class' : div_classes[1]})[1])
-    anime_divs.append(soup.find_all('div',{'class' : div_classes[0]})[1])
-    anime_divs.append(soup.find_all('div',{'class' : div_classes[3]})[0])
-    anime_divs.append(soup.find_all('div',{'class' : div_classes[0]})[2])
-    anime_divs.append(soup.find_all('div',{'class' : div_classes[1]})[2])
-    anime_divs.append(soup.find_all('div',{'class' : div_classes[2]})[1])
-    anime_divs.append(soup.find_all('div',{'class' : div_classes[1]})[3])
-    for anime_div in anime_divs:
-        recent_anime_info = {
-            'link' : str(anime_div.find('a',{'class' : 'an'}).get('href')), 
-            'name' : str(anime_div.find('div',{'class' : 'title-box-2'}).text),
-            'episode_num' : str(anime_div.find('div',{'class' : 'centerv2'}).text.split('/')[0][:-2].strip()),
-            'image_link' : str(anime_div.find('div',{'class' : 'image-box'}).find('img').get('src'))
-        }
-        recent_anime.append(recent_anime_info)
-    return recent_anime
+    anime_list = soup.find('ul',{'class' : 'items'}).find_all('li')
+    for anime in anime_list:
+        recent_anime = {'episode_num' : anime.find('p',{'class':'episode'}).text,
+                        'image_link' : anime.find('img').get('src'),
+                        'link' : ROOT_URL + anime.find('a').get('href'),
+                        'name' : anime.find('p',{'class' : 'name'}).text
+                        }
+        recent_animes.append(recent_anime)
+    
+    return recent_animes
 
 def get_popular_anime():
-    url = ROOT_URL + str('/popular')
+    url = ROOT_URL + str('/popular.html')
     response = requests.get(url)
     soup = BeautifulSoup(response.content)
-    animes = soup.findAll('a',{'class':'an'})[:10]
+    anime_list = soup.find('ul',{'class':'items'}).find_all('li')[:10]
     popular_animes = []
-    for anime in animes:
-        popular_anime = {
-            'image_link' : str(anime.find('div',{'class':'similarpic'}).find('img').get('src')),
-            'name' : str(anime.find('div',{'class':'similardd'}).text)[3:].strip(),
-            'anime_link' : str(anime.get('href'))
-        }
+    for anime in anime_list:
+        popular_anime = {'name' : anime.find('p',{'class' : 'name'}).text,
+                        'anime_link' : ROOT_URL + anime.find('p',{'class' : 'name'}).find('a').get('href'),
+                        'image_link' : anime.find('img').get('src')}
         popular_animes.append(popular_anime)
     return popular_animes
 
 def get_anime_desc(url):
+    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,chrome_options=chrome_options)
     categories = []
     episode_links = []
     episode_names = []
     response = requests.get(url)
     soup = BeautifulSoup(response.content)
-    description = soup.find('div',{'class' : 'infodes2'}).text
-    category_tags = soup.findAll('div',{'class' : 'infodes2'})[1].find('div',{'class':'textc'}).findAll('a',{'class':'infoan'})
-    episode_tags = soup.find('div',{'class' : 'infoepbox'}).findAll('a',{'class':'infovan'})
-
-    for episode_tag in episode_tags:
-        episode_link = episode_tag.get('href')
-        episode_links.append('/'+episode_link)
-        episode_name = episode_tag.find('div',{'class':'infoept2'}).find('div',{'class':'centerv'}).text.strip()
-        episode_names.append(episode_name)
+    description = soup.find('div',{'class' : 'anime_info_body_bg'}).find_all('p',{'class':'type'})[1].text
+    category_tags = soup.find('div',{'class' : 'anime_info_body_bg'}).find_all('p',{'class':'type'})[2].findAll('a')
+    driver.get(url)
+    time.sleep(3)
+    episode_list = driver.find_element_by_id('episode_page')
+    episode_list_items = episode_list.find_elements_by_tag_name('li') 
+    for episode_list_item in reversed(episode_list_items):
+        episode_list_item.click()
+        time.sleep(3)
+        soup = BeautifulSoup(driver.page_source)
+        episodes = soup.find('ul',{'id':'episode_related'}).findAll('li')
+        for episode in episodes:
+            episode_link = ROOT_URL + (episode.find('a').get('href')).strip()
+            episode_links.append(episode_link)
+            episode_name = episode.find('div',{'class' : 'name'}).text
+            episode_names.append(episode_name)
     for category_tag in category_tags:
-        categories.append(category_tag.text)
+        categories.append((category_tag.text).split(' ')[-1])
     info =  {'desc' : description.strip(),'categories':categories,'episode_links':episode_links,'episode_names':episode_names}
+    driver.quit()
     return info
-
-def get_video_link(url):
-    browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,chrome_options=chrome_options)
-    browser.get(url)
-    elem = browser.find_element_by_xpath('//*[@id="main"]/div[7]/div')
-    elem.click()
-    browser.switch_to.window(browser.window_handles[1])
-    iframe = browser.find_elements_by_tag_name('iframe')[0]
-    browser.switch_to_frame(iframe)
-    buttons = browser.find_elements_by_xpath("//*[contains(text(), 'Download Xstreamcdn')]")
-    buttons[0].click()
-    browser.switch_to.window(browser.window_handles[2])
-    video_link = browser.current_url
-    link_parts = video_link.split('/')
-    first_part = '/'.join(link_parts[:-2])
-    last_part = link_parts[-1]
-    video_link = {'video_link' : first_part+'/v/'+last_part}
-    browser.quit()
-    return video_link
-
+#get_anime_desc("https://www17.gogoanime.io/category/boruto-naruto-next-generations")
 
 app = Flask(__name__)
-
-@app.route('/video_link')
-def fetch_video_link():
-    url = request.args.get('url')
-    response = get_video_link(url)
-    if response:
-        api_response = make_response(jsonify(response),200)
-    api_response.headers['Content-Type'] = 'application/json'
-    return api_response
 
 @app.route('/recent_anime')
 def fetch_recent_anime():
