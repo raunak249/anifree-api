8c389ad5735014965c8c30e0e6329cef61072411
Raunak Adak <raunak249@gmail.com>
2020-04-09 21:29:08 +0530

    source changed to MAL

--- anime_api.py
+++ anime_api.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 import os
 
 
-ROOT_URL = 'https://www.gogoanime.io'
+ROOT_URL = 'https://myanimelist.net'
 CHROMEDRIVER_PATH = os.environ.get("CHROMEDRIVER_PATH")
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--start-maximized")
@@ -20,19 +20,24 @@ chrome_options.binary_location = '/app/.apt/usr/bin/google-chrome'
 #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
 
+def good_image(url):
+    lst = url.split('/')
+    del lst[3:5]
+    url = '/'.join(lst)
+    return url
 
 def search_anime(anime_name):
-    search_url = 'https://www17.gogoanime.io//search.html?keyword=' + str(anime_name)
+    search_url = 'https://myanimelist.net/search/all?q=' + str(anime_name)
     response = requests.get(search_url)
     soup = BeautifulSoup(response.content)
     search_results = []
-    anime_list = soup.find('ul',{'class' : 'items'}).find_all('li')
+    anime_list = soup.findAll('article')[0].findAll('div',{'class':'list di-t w100'})
     for anime in anime_list:
-        recent_anime = {'image_link' : anime.find('img').get('src'),
-                        'link' : ROOT_URL + anime.find('a').get('href'),
-                        'name' : anime.find('p',{'class' : 'name'}).text
+        search_result = {'image_link' : good_image(anime.find('div',{'class':'picSurround di-tc thumb'}).find('img').get('data-src')),
+                        'link' : anime.find('div',{'class':'picSurround di-tc thumb'}).find('a').get('href'),
+                        'name' : anime.find('div',{'class':'information di-tc va-t pt4 pl8'}).find('a').text
                         }
-        search_results.append(recent_anime)
+        search_results.append(search_result)
     return search_results
 
 def get_recent_anime():
@@ -40,57 +45,41 @@ def get_recent_anime():
     url = ROOT_URL
     response = requests.get(url)
     soup = BeautifulSoup(response.content)
-    anime_list = soup.find('ul',{'class' : 'items'}).find_all('li')
+    anime_list = soup.findAll('div',{'class' : 'widget-slide-outer'})[1].findAll('li',{'class' : 'btn-anime episode'})
     for anime in anime_list:
-        recent_anime = {'episode_num' : anime.find('p',{'class':'episode'}).text,
-                        'image_link' : anime.find('img').get('src'),
-                        'link' : ROOT_URL + anime.find('a').get('href'),
-                        'name' : anime.find('p',{'class' : 'name'}).text
+        recent_anime = {'episode_num' : anime.find('div',{'class':'link episode js-widget-episode-video-link'}).find('div',{'class' : 'title di-b'}).find('a').text,
+                        'image_link' : good_image(anime.find('div',{'class':'link episode js-widget-episode-video-link'}).find('img').get('data-src')),
+                        'name' : anime.find('div',{'class':'link episode js-widget-episode-video-link'}).get('data-title')
                         }
         recent_animes.append(recent_anime)
     
     return recent_animes
 
 def get_popular_anime():
-    url = ROOT_URL + str('/popular.html')
+    url = ROOT_URL + '/topanime.php?type=airing'
     response = requests.get(url)
     soup = BeautifulSoup(response.content)
-    anime_list = soup.find('ul',{'class':'items'}).find_all('li')[:10]
-    popular_animes = []
+    popular_animes =[]
+    anime_list = soup.find('table',{'class' : 'top-ranking-table'}).findAll('tr',{'class':'ranking-list'})
     for anime in anime_list:
-        popular_anime = {'name' : anime.find('p',{'class' : 'name'}).text,
-                        'anime_link' : ROOT_URL + anime.find('p',{'class' : 'name'}).find('a').get('href'),
-                        'image_link' : anime.find('img').get('src')}
+        popular_anime = {'name' : anime.find('td',{'class' : 'title al va-t word-break'}).find('div',{'class':'detail'}).find('div',{'class' :'di-ib clearfix'}).find('a').text,
+                        'anime_link' : anime.find('td',{'class' : 'title al va-t word-break'}).find('a').get('href'),
+                        'image_link' : good_image(anime.find('td',{'class' : 'title al va-t word-break'}).find('a').find('img').get('data-src'))}
         popular_animes.append(popular_anime)
     return popular_animes
 
 def get_anime_desc(url):
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,chrome_options=chrome_options)
     categories = []
-    episode_links = []
     episode_names = []
     response = requests.get(url)
     soup = BeautifulSoup(response.content)
-    description = soup.find('div',{'class' : 'anime_info_body_bg'}).find_all('p',{'class':'type'})[1].text
-    category_tags = soup.find('div',{'class' : 'anime_info_body_bg'}).find_all('p',{'class':'type'})[2].findAll('a')
-    driver.get(url)
-    time.sleep(3)
-    episode_list = driver.find_element_by_id('episode_page')
-    episode_list_items = episode_list.find_elements_by_tag_name('li') 
-    for episode_list_item in reversed(episode_list_items):
-        episode_list_item.click()
-        time.sleep(3)
-        soup = BeautifulSoup(driver.page_source)
-        episodes = soup.find('ul',{'id':'episode_related'}).findAll('li')
-        for episode in episodes:
-            episode_link = ROOT_URL + (episode.find('a').get('href')).strip()
-            episode_links.append(episode_link)
-            episode_name = episode.find('div',{'class' : 'name'}).text
-            episode_names.append(episode_name)
-    for category_tag in category_tags:
-        categories.append((category_tag.text).split(' ')[-1])
-    info =  {'desc' : description.strip(),'categories':categories,'episode_links':episode_links,'episode_names':episode_names}
-    driver.quit()
+    description = soup.find('span',{'itemprop' : 'description'}).text
+    # category_tags = soup.find('div',{'class' : 'anime_info_body_bg'}).find_all('p',{'class':'type'})[2].findAll('a')
+    response = requests.get(url+'/episode')
+    soup = BeautifulSoup(response.content)
+    episodes = soup.find('span',{'class' : 'di-ib pl4 fw-n fs10'}).text.split('/')[0][1:]
+    episode_names = ['Episode '+ str(episode_name) for episode_name in range(1,int(episodes)+1)]
+    info =  {'desc' : str(description),'episode_names':episode_names}
     return info
 
 def get_video_link(url):
